44d489347f500a183cc35ac1f3b2112a2c139d4d
kukufuckingkan <keitakukukhan@gmail.com>
2024-10-18 23:12:01 +0000

    sasas

---
+++ src/main/java/gn/moria/mani/ManiApplication.java
@@ -0,0 +1,13 @@
+package gn.moria.mani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManiApplication {
+    // user authentication , authorization, api
+    public static void main(String[] args) {
+        SpringApplication.run(ManiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gn/moria/mani/ManiApplicationTests.java
@@ -0,0 +1,13 @@
+package gn.moria.mani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
